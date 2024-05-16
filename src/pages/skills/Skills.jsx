db622217d6d080fbb79fc4d927de2057a037db2a
binumarcodes <abdulrahmanbinumar179@gmail.com>
2024-05-16 10:36:19 +0100

    Deployment update

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import skillsstyle from "./Skills.module.css"
+import Skills from "../../../src/assets/Skills.png" 
 
 function Skills() {
   return (
     <div className={skillsstyle["container"]}>
-      <img src="../src/assets/Skills.png" alt="" className={skillsstyle["img"]} />
+      <img src={me} alt="" className={skillsstyle["img"]} />
       <div id='projects' className={skillsstyle["projects-id"]}></div>
     </div>
   )
