4d94918c8f5181a87c92ade173d933741fa6ff04
binumarcodes <abdulrahmanbinumar179@gmail.com>
2024-05-16 10:17:53 +0100

    Deployment update

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,5 @@ export default defineConfig({
   build: {
     outDir: 'build', // Specify the output directory for the build
   },
+  base: '/Portfoilo/' // Set the base URL to match your GitHub Pages repository name
 })
