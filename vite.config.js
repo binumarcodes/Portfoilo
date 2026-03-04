819c588ca9055c56ce78a6e8a9cf8d07799bc002
binumarcodes <abdulrahmanbinumar179@gmail.com>
2026-03-04 18:20:58 +0100

    Switch fully to Vercel config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // Specify the output directory for the build
-  },
-  base: '/Portfoilo/' // Set the base URL to match your GitHub Pages repository name
 })
